2cc61a0987d6fabd53dcadb94ad41fa165db5447
Kristi Rakipllari <kristi.rakipllari@gmail.com>
2025-04-07 11:35:10 +0200

    Projects change

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -22,24 +22,6 @@ export const Projects = () => {
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                     <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-blue-500/30 
                                         hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-4">Email spam/ham </h3>
-                        <p className="text-gray-300 mb-4">Group project. Built a model to classify emails as either spam or ham (non-spam). The dataset was sourced from Kaggle, containing labeled email messages, which were used to train and evaluate a classification model.</p>
-                        <div className="flex flex-wrap gap-2">
-                            {firstprojectSkills.map((tech, key) => (
-                            <span
-                                key={key}
-                                className="bg-blue-500/10 text-blue-500 py-1 px-4 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,1)] transition"
-                            >
-                                {tech}
-                            </span>
-                            ))}
-                        </div>
-                            <div className="flex items-center justify-between mt-4">
-                                <a href="#" target='blank' className="text-blue-400 hover:underline">View Project →</a>
-                            </div>
-                    </div>
-                    <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-blue-500/30 
-                                        hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                         <h3 className="text-xl font-bold mb-4">
                             Twitter Climate Change - Sentiment Analysis
                         </h3>
@@ -94,7 +76,24 @@ export const Projects = () => {
                                 <a href="https://kristirakipllari.github.io/kristi-web-portfolio/" target='blank' className="text-blue-400 hover:underline">View Project →</a>
                             </div>
                     </div>
-
+                    <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-blue-500/30 
+                                        hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                        <h3 className="text-xl font-bold mb-4">Email spam/ham </h3>
+                        <p className="text-gray-300 mb-4">Group project. Built a model to classify emails as either spam or ham (non-spam). The dataset was sourced from Kaggle, containing labeled email messages, which were used to train and evaluate a classification model.</p>
+                        <div className="flex flex-wrap gap-2">
+                            {firstprojectSkills.map((tech, key) => (
+                            <span
+                                key={key}
+                                className="bg-blue-500/10 text-blue-500 py-1 px-4 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,1)] transition"
+                            >
+                                {tech}
+                            </span>
+                            ))}
+                        </div>
+                            <div className="flex items-center justify-between mt-4">
+                                <a href="#" target='blank' className="text-blue-400 hover:underline">View Project →</a>
+                            </div>
+                    </div>                
                 </div>
 
             </div>
