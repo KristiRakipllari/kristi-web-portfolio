84ad4481006ad6c7e391b2ea49377cbc9c0a7b25
Kristi Rakipllari <kristi.rakipllari@gmail.com>
2025-05-04 01:43:25 +0200

    New Project Update

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -44,7 +44,7 @@ export const Projects = () => {
                     <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-blue-500/30 
                                         hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                         <h3 className="text-xl font-bold mb-4">Shortify AI Summarizer</h3>
-                        <p className="text-gray-300 mb-4">An AI-powered summarization web app that extracts and shortens content from articles and web pages. Used Article Extractor and Summarizer API from Rapidapi </p>
+                        <p className="text-gray-300 mb-4">An AI-powered summarization web app that extracts and shortens content from articles and web pages. Used Article Extractor and Summarizer API from Rapidapi. </p>
                         <div className="flex flex-wrap gap-2">
                             {fifthprojectSkills.map((tech, key) => (
                             <span
